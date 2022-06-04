0ba8c32b59875e735d677b542b98686c0e23ba77
JiaweiLu1999 <jl5999@columbia.edu>
2022-06-03 23:35:34 -0400

    update

--- src/Comment.java
+++ src/Comment.java
@@ -2,9 +2,11 @@
  * @author Jiawei Lu
  * @version 1.0
  */
-
 public class Comment {
     public static void main(String[] args) {
-        System.out.println("Hello Java8!");
+        double a = 11.2;
+        char b = 's';
+        String c = "hello";
+        System.out.println(c);
     }
 }
