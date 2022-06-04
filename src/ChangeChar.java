fef4693cff6009d2b3a51869ed07e50603cc4ad7
JiaweiLu1999 <jl5999@columbia.edu>
2022-06-03 21:55:44 -0400

    add src

---
+++ src/ChangeChar.java
@@ -0,0 +1,11 @@
+public class ChangeChar {
+	/* 	comment
+		comment
+	*/
+	public static void main(String[] args) {
+		// commet
+		System.out.println("ml\tdl\tnlp");
+		System.out.println("ml\ndl\nnlp");
+		System.out.println("ml\rdlww\rn");
+	}
+}
---
+++ src/Comment.java
@@ -0,0 +1,10 @@
+/**
+ * @author Jiawei Lu
+ * @version 1.0
+ */
+
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("Hello Java8!");
+    }
+}
