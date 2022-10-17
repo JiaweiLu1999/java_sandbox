00a9f9390113d860f274803f076622478140de13
Jiawei Lu <jl5999@columbia.edu>
2022-10-16 23:24:31 -0400

    update 1016

--- Hello/src/com/regexp/Regexp.java
+++ Hello/src/com/regexp/Regexp.java
@@ -6,14 +6,18 @@ import java.util.regex.Pattern;
 public class Regexp {
 
     public static void main(String[] args) {
-        String content = "123456, abc efg";
+        String content = "III...I wantwant... tototo...to learn Java";
 
-        Pattern pattern =  Pattern.compile("[a-zA-Z]+");
+        Pattern pattern =  Pattern.compile("\\.");
         Matcher matcher = pattern.matcher(content);
+        content = matcher.replaceAll("");
+        System.out.println(content);
 
-        while (matcher.find()) {
-            System.out.println(matcher.group());
-        }
+        pattern = Pattern.compile("(\\w+?)\\1+");
+        matcher = pattern.matcher(content);
+
+        content = matcher.replaceAll("$1");
+        System.out.println(content);
     }
 
 }
